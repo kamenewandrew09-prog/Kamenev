c4750b95ded1d1518af31f63b194eb302f6e74ef
kamenewandrew09-prog <kamenew.andrew09@gmail.com>
2026-01-09 17:36:52 +0200

    hm34 commit

--- script.js
+++ script.js
@@ -3,6 +3,16 @@
 // повертає число
 // countChar("banana", "a") // 3
 
-const countChar = str => {
-    
+const countChar = (str, char) => {
+    let count = 0;
+
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] === char) {
+            count++
+        }
+
+    }   
+    return count++;
 }
+
+console.log(countChar("banana", "a"));
