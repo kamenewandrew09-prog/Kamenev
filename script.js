2bb51626db30dbb024657a13bac450ebef18bb45
kamenewandrew09-prog <kamenew.andrew09@gmail.com>
2026-01-21 15:10:45 +0200

    hw34 commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// 1. Написати функцію checkLogin(login), яка:
+// приймає рядок
+// повертає true, якщо:
+// довжина логіну символів більше 8
+// інакше повертає false
+
+
+// 2. Написати функцію replaceA(str), яка:
+// замінює
+// повертає новий рядок
+// replaceA("banana") // "b@n@n@"
+
+
+
+// 3. Написати функцію isEmail(email), яка повертає true, якщо:
+// рядок містить символ @
+// після @ є крапка .
+// @ не на початку рядка
+// . не останній символ
+// Подивіться сюди: includes(), indexOf(), lastIndexOf()
+
+
+// 4. Написати функцію trimText(str), яка:
+// видаляє
+// повертає новий рядок
+// trimText("   hello world ") // "hello world"
+
+
+
+//  5.Написати функцію capitalize(str), яка:
+// робить
+// повертає новий рядок
+// capitalize("javascript is a language") // "Javascript Is A Language"
+
+// toUpperCase(), slice(), split(), join()
+
+
+// 6. Написати функцію countChar(str, char), яка:
+// рахує,
+// повертає число
+// countChar("banana", "a") // 3
