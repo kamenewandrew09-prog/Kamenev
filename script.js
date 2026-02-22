94e7daead6859dfaca54a44fe23c580dcd20cf0c
kamenewandrew09-prog <kamenew.andrew09@gmail.com>
2026-02-22 14:20:57 +0200

    hw41 commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function Player(name, power, hp, type='ai') {
+function Player(name, power, defence, hp, type='ai') {
     this.name = name;
     this.power = power;
+    this.defence = defence;
     this.hp = hp;
+    this.maxHp = hp;
     this.type = type;
 
     this.directionOfAttack = null;
     this.directionOfDefence = null;
 
     this.attack = function() {
+
         return this.power;
     }
 
@@ -17,29 +20,56 @@ function Player(name, power, hp, type='ai') {
 }
 
 const players = [
-    new Player('Play1', 10, 100, 'user'),
-    new Player('Lvl01', 2, 20, 'comp_lvl_01'),
+    new Player('Player 1', 10, 13, 100, 'user'),
+    new Player('AI 01', 2, 13, 20, 'comp_lvl_01'),
 ]
 
 const gameController = {
     attackTurn: null,
     defenceTurn: null,
+    playerCards: null,
+    playersHP: null,
+
+    resizeHealthBar() {
+        for (const i = 0; i <=1; i++) {
+            this.playersHP[i].style.width = `${players[i].hp / players[i].maxHp * 100}%`
+        }
+    },
+
+    addHTMLData() {
+        this.playerCards = document.querySelectorAll('.player-card');
+        this.playersHP = document.querySelectorAll('.hp-bar-inner');
+        for (let i = 0; i <=1; i++) {
+            const playerNameEl = this.playerCards[i].querySelector('.player-stats-item.name');
+            playerNameEl.innerText = players[i].name;
+
+            const playerPowerEl = this.playerCards[i].querySelector('.player-stats-item.power span');
+            playerPowerEl.innerText = players[i].power;
+
+            const playerDefenceEl = this.playerCards[i].querySelector('.player-stats-item.defence span');
+            playerDefenceEl.innerText = players[i].defence;
+
+            
+        }
+    },
 
     start() {
+        this.addHTMLData();
+        this.resizeHealthBar();
+
         this.attackTurn = Math.round(Math.random()); //визначаємо чий хід
         this.defenceTurn = Math.abs(this.attackTurn - 1);
 
         while (players[0].hp > 0 && players[1].hp > 0) {
-            alert(`атакує гравець ${players[this.attackTurn].name}`)
-            alert(`${players[this.attackTurn].name} наносить ${players[this.attackTurn].power} урона`)
-            // реалізація механіки 
-            // викликаєте метод отримати урон у захисника та передаєте в нього
-            // метод атаки у атакуючого
+            // alert(`атакує гравець ${players[this.attackTurn].name}`)
+            // alert(`${players[this.attackTurn].name} наносить ${players[this.attackTurn].power} урона`);
+            // this.resizeHealthBar();
+            
             players[this.defenceTurn].takeDamage(players[this.attackTurn].attack())
-            this.showHealth();
+            // this.showHealth();
             this.changeTurn();
         }
-        this.showWinner();
+        // this.showWinner();
     }, 
     changeTurn() {
         this.attackTurn = this.defenceTurn;
@@ -61,23 +91,3 @@ const gameController = {
 }
 
 gameController.start();
-
-
-// ДЗ 2
-let counterZeros = 0
-let counterOnes = 0
-
-for(let i = 0; i < 1000; i++) {
-    let turn = Math.round(Math.random());
-    if(turn === 0) {
-        counterZeros++
-    } else {
-        counterOnes++
-    }
-
-}
-
-console.log(counterZeros)
-console.log(counterOnes)
-
-
