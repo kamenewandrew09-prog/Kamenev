10ab6b9785922317f201ab6eb45eb4235d7d3164
kamenewandrew09-prog <kamenew.andrew09@gmail.com>
2026-04-17 16:53:57 +0300

    hw49 commit

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
-// const grades = {
-//   math: [10, 9, 11, 10],
-//   ukrainian: [11, 10, 12, 11],
-//   english: [9, 8, 10, 9],
-//   history: [8, 7, 9, 8],
-//   physics: [10, 9, 10, 11],
-//   chemistry: [7, 6, 8, 7],
-//   biology: [9, 8, 10, 9],
-//   geography: [8, 7, 9, 8],
-//   informatics: [12, 11, 12, 12],
-//   pe: [11, 10, 12, 11]
-// };
+const grades = {
+  math: [10, 9, 11, 10],
+  ukrainian: [11, 10, 12, 11],
+  english: [9, 8, 10, 9],
+  history: [8, 7, 9, 8],
+  physics: [10, 9, 10, 11],
+  chemistry: [7, 6, 8, 7],
+  biology: [9, 8, 10, 9],
+  geography: [8, 7, 9, 8],
+  informatics: [12, 11, 12, 12],
+  pe: [11, 10, 12, 11]
+};
 
-// const elValues = Object.entries(grades)
-// const newObj = {}
+const elValues = Object.entries(grades)
+const newObj = {}
 
-// for(const el of elValues) {
-//   const subject = el[0]
-//   const gradesArr = el[1]
-//   let sum = 0
-//   for(const grade of gradesArr) {
-//     sum += grade
-//   }
-//   const average = sum / gradesArr.length
+for(const el of elValues) {
+  const subject = el[0]
+  const gradesArr = el[1]
+  let sum = 0
+  for(const grade of gradesArr) {
+    sum += grade
+  }
+  const average = sum / gradesArr.length
 
-//   const rounded = Math.round(average * 10) / 10
-//   newObj[subject] = rounded
-// }
+  const rounded = Math.round(average * 10) / 10
+  newObj[subject] = rounded
+}
 
-// console.log(newObj);
+console.log(newObj);
 
-// for (const el of Object.entries(newObj)) {
-//   const subject = el[0];
-//   const average = el[1];
+for (const el of Object.entries(newObj)) {
+  const subject = el[0];
+  const average = el[1];
 
-//   document.body.insertAdjacentHTML(
-//     'beforeend',
-//     `<p><u>${subject}</u>: <b>${average}</b></p>`
-//   );
-// }
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `<p><u>${subject}</u>: <b>${average}</b></p>`
+  );
+}
 
-// for (const key of Object.keys(newObj)) {
-//   const average = newObj[key];
+for (const key of Object.keys(newObj)) {
+  const average = newObj[key];
 
-//   document.body.insertAdjacentHTML(
-//     'beforeend',
-//     `<p><u>${key}</u>: <b>${average}</b></p>`
-//   );
-// }
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `<p><u>${key}</u>: <b>${average}</b></p>`
+  );
+}
 
-const someDate = new Date()
-const someDate1 = new Date('2026-01-01')
-console.log((someDate - someDate1) / 1000 / 60 / 60 / 24)
+
 
 
 
