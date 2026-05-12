759aa84809e9ffc5820a63fcc139bbd68fc2ff90
kamenewandrew09-prog <kamenew.andrew09@gmail.com>
2026-05-12 18:08:48 +0300

    hw54-55  commit

--- script.js
+++ script.js
@@ -1,53 +1,70 @@
-// // 1.
-// function byLetter(str) { 
-//     if(str === '') return
-//     console.log(str[0])
-//     byLetter(str.slice(1))
-// }
+// 1.
+function byLetter(str) { 
+    if(str === '') return
+    console.log(str[0])
+    byLetter(str.slice(1))
+}
 
-// byLetter('Hello')
+byLetter('Hello')
 
-// // 2.
+// 2.
 
-// function sumDigits(n) {
-//     if(n < 10) {
-//         return n 
-//     }
-//     return (n % 10) + sumDigits(Math.floor(n / 10))
-// }
+function sumDigits(n) {
+    if(n < 10) {
+        return n 
+    }
+    return (n % 10) + sumDigits(Math.floor(n / 10))
+}
 
-// console.log(sumDigits(1234))
+console.log(sumDigits(1234))
 
-// // 4.
 
-// function findMin(arr) {
-//     if(arr.length === 1) {
-//         return arr[0]
-//     }
+// 3.
 
-//     const restMin = findMin(arr.slice(1))
-//     if (arr[0] < restMin) {
-//         return arr[0]
-//     } return restMin
-// }
+function flatten(arr) {
+    let result = [];
 
-// console.log(findMin([5,3,8,6]))
-
-function createDownCounter(start) {
-    let counter = start
-
-    return function() {
-        const current = counter 
-        if(counter > 0) {
-            counter--;
+    for (let el of arr) {
+        if (Array.isArray(el)) {
+            result = result.concat(flatten(el));
+        } else {
+            result.push(el);
         }
-        return current;
+    }
+
+    return result;
+}
+
+console.log(flatten([1, [2, 3], 4]));
+
+// 4.
+
+function findMin(arr) {
+    if(arr.length === 1) {
+        return arr[0]
+    }
+
+    const restMin = findMin(arr.slice(1))
+    if (arr[0] < restMin) {
+        return arr[0]
+    } return restMin
+}
+
+console.log(findMin([5,3,8,6]))
+
+
+// 5.
+
+function countOccurrences(arr, el) {
+    if(arr.length === 0) {
+        return 0
+    }
+    if(arr[0] === el) {
+        return 1 + countOccurrences(arr.slice(1), el)
+    } else {
+        return countOccurrences(arr.slice(1), el)
     }
 }
 
-const counter = createDownCounter(3)
+console.log(countOccurrences([1,2,4,3,4], 4))
 
-console.log(counter())
-console.log(counter())
-console.log(counter())
-console.log(counter())
