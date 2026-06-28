b9cd7b2a3d04ebadfae8a856db58b5eefd2a6427
kamenewandrew09-prog <kamenew.andrew09@gmail.com>
2026-06-28 13:29:56 +0300

    hw64  commit

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
-class Character {
-    #health
-    #damage
-
-    constructor(health, damage) {
-        this.#health = health
-        this.#damage = damage
-    }
-
-    takeDamage(value) {
-        this.#health -= value
-        return this.#health;
-    }
-
-    isAlive() {
-        if(this.#health > 0) {
-            return true
-        } 
-        return false 
-    }
-    get damage() {
-        return this.#damage
-    }
-}
-
-class Hero extends Character {
-    name
-
-    constructor(name, health, damage) {
-        super(health, damage)
-        this.name = name
-    }
-
-    attack(target) {
-        return target.takeDamage(this.damage)
-    }
-}
-
-
-const player = new Hero("player", 100, 20)
-const enemy = new Hero("enemy", 45, 30)
-
-console.log(player.attack(enemy));
-console.log(enemy.attack(player));
-
+new Promise((resolve, reject) => {
+    resolve(10)
+})
+    .then((sum) => {
+        return sum + 15
+    })
+    .then((multiply) => {
+        return multiply * 2
+    })
+    .then((divide) => {
+        return divide / 5
+    })
+    .then((minus) => {
+        return minus - 3
+    })
+    .then((result) => {
+        console.log(result)
+    })
