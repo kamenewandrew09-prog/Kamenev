6e8dcc673edb4e0ee95dffc3a7a761cfcaf3e236
kamenewandrew09-prog <kamenew.andrew09@gmail.com>
2026-06-28 12:50:33 +0300

    hw63  commit

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-class User {
-    static userID = 0;
-    static users = []
+class Character {
+    #health
+    #damage
 
-    constructor(login) {
-        User.users.forEach(user => {
-            if (user.login.toLowerCase() === login.toLowerCase()) {
-                throw new Error('login already exists');
-            }
-        });
-
-        this.id = User.userID++;
-        this.login = login;
-       
-        User.users.push(this);
+    constructor(health, damage) {
+        this.#health = health
+        this.#damage = damage
     }
 
-    sayHi() {
-        console.log(`Hello from ${this.login}`)
+    takeDamage(value) {
+        this.#health -= value
+        return this.#health;
     }
 
-    static greeting() {
-        User.users.forEach(user => {
-            user.sayHi();
-        });
+    isAlive() {
+        if(this.#health > 0) {
+            return true
+        } 
+        return false 
+    }
+    get damage() {
+        return this.#damage
     }
 }
 
-const u1 = new User('wqe');
-const u2 = new User('asd');
-const u3 = new User('W1qe');
+class Hero extends Character {
+    name
+
+    constructor(name, health, damage) {
+        super(health, damage)
+        this.name = name
+    }
+
+    attack(target) {
+        return target.takeDamage(this.damage)
+    }
+}
 
 
-User.greeting();
+const player = new Hero("player", 100, 20)
+const enemy = new Hero("enemy", 45, 30)
+
+console.log(player.attack(enemy));
+console.log(enemy.attack(player));
+
