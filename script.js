6189120b4a9451efb3e8c19984e5b59edf0d726c
kamenewandrew09-prog <kamenew.andrew09@gmail.com>
2026-07-04 13:02:28 +0300

    hw65  commit

--- script.js
+++ script.js
@@ -1,18 +1,88 @@
-new Promise((resolve, reject) => {
-    resolve(10)
+function bulka(burgerArr, callbackFn) {
+
+    setTimeout(() => {
+        console.log("булочка розігріта");
+        burgerArr.push("булочка")
+        callbackFn(burgerArr)
+    }, 2000)
+}
+
+function kotleta(burgerArr, callbackFn) {
+   
+    setTimeout(() => {
+        console.log("котлета посмажена");
+        burgerArr.push("котлета")
+        callbackFn(burgerArr)
+    }, 2500)
+}
+
+function lettuce(burgerArr, callbackFn) {
+   
+    setTimeout(() => {
+        console.log("поклали листя салату");
+        burgerArr.push("листя салату")
+        callbackFn(burgerArr)
+    }, 3000)
+}
+
+function cheese(burgerArr, callbackFn) {
+   
+    setTimeout(() => {
+        console.log("ломтик сиру");
+        burgerArr.push("сир")
+        callbackFn(burgerArr)
+    }, 3500)
+}
+
+function sauce(burgerArr, callbackFn) {
+   
+    setTimeout(() => {
+        console.log("смазали соусом");
+        burgerArr.push("соус")
+        callbackFn(burgerArr)
+    }, 4000)
+}
+
+function zbirka(burgerArr, callbackFn) {
+
+    setTimeout(() => {
+        console.log("бургер зібраний " + burgerArr);
+        callbackFn(burgerArr);
+    }, 1000)
+}
+
+
+new Promise((resolve) => {
+    const burgerArr = []
+    bulka(burgerArr, resolve);
+   
 })
-    .then((sum) => {
-        return sum + 15
+   .then(burgerArr => {
+        return new Promise(resolve => {
+            kotleta(burgerArr, resolve);
+        })
     })
-    .then((multiply) => {
-        return multiply * 2
+    .then(burgerArr => {
+        return new Promise(resolve => {
+            lettuce(burgerArr, resolve);
+        })
     })
-    .then((divide) => {
-        return divide / 5
+    .then(burgerArr => {
+        return new Promise(resolve => {
+            cheese(burgerArr, resolve);
+        })
     })
-    .then((minus) => {
-        return minus - 3
+    .then(burgerArr => {
+        return new Promise(resolve => {
+            sauce(burgerArr, resolve);
+        })
     })
-    .then((result) => {
+    .then(burgerArr => {
+        return new Promise(resolve => {
+            zbirka(burgerArr, resolve);
+        })
+    })
+    .then(result => {
         console.log(result)
+        console.log("ГОТОВО");
     })
