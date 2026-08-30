8ad18ab9a2838669098a18f219c4c33a760d6d7d
kamenewandrew09-prog <kamenew.andrew09@gmail.com>
2026-08-30 16:40:53 +0300

    hw70  commit

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+const pagination = document.querySelector('.pagination')
 const prevPageBtnDOM = document.querySelector('.prev-page')
 const currPageDOM = document.querySelector('.curr-page')
 const nextPageBtnDOM = document.querySelector('.next-page')
 const loadMoreBtnDOM = document.querySelector('.load-more')
+const wrapper = document.querySelector('.wrapper')
 const mainDOM = document.querySelector('.main')
+const searchForm = document.querySelector('.search-form')
+const searchForm2 = document.querySelector('.search-form-2')
+const searchFormAjax = document.querySelector('.search-form-ajax')
 const options = {
     method: 'GET',
     headers: {
@@ -16,7 +21,9 @@ const imagePrefix = 'https://image.tmdb.org/t/p/w342/'
 const genresObj = {}
 let currentPage = 1
 let totalPages;
-let isLoading = false 
+let isLoading = false
+let searchMovieCooldown; 
+let searchMovieAjaxCooldown; 
 
 async function getTopRatedMovies(page, isClearPage = true) {
     try {
@@ -56,6 +63,75 @@ async function getTopRatedMovies(page, isClearPage = true) {
     }
 }
 
+
+// function searchMovie(keyword) {
+//     currentPage = 1
+//     fetch(`https://api.themoviedb.org/3/search/movie?include_adult=true&language=uk-UK&page=${currentPage}&query=${keyword}`, options)
+//       .then(res => res.json())
+//       .then(res => {
+//         console.log(res)
+//         mainDOM.innerHTML = ''
+//         mainDOM.classList.add('list')
+//       })
+//       .catch(err => console.error(err));
+// }
+
+function searchMovieAjax(keyword) {
+    const proposeWrapper = searchFormAjax.querySelector('.propose-wrapper')
+    clearTimeout(searchMovieAjaxCooldown)
+    if(keyword.length >= 2) {
+        searchMovieAjaxCooldown = setTimeout(() => {
+            fetch(`https://api.themoviedb.org/3/search/movie?include_adult=true&language=uk-UK&query=${keyword}`, options)
+            .then(res => res.json())
+            .then(res => {
+                const movies = res.results
+                console.log(res)
+                proposeWrapper.innerHTML = ''
+                if(!movies.length){
+                    proposeWrapper.classList.add('hidden')
+                } else {
+                    proposeWrapper.classList.remove('hidden')
+                }
+                movies.forEach(movie => {
+                    proposeWrapper.insertAdjacentHTML('beforeend', `
+                        <div class="search-item-ajax" onclick="getDetails(${movie.id})">${movie.title}</div>
+                    `)
+                })
+            })
+            .catch(err => console.error(err));
+        }, 1000)
+    } else {
+        proposeWrapper.innerHTML = ''
+        proposeWrapper.classList.add('hidden')
+    } 
+}
+
+
+searchForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const form = event.target
+    const searchMovieInput = form.querySelector('.search-movie-input')
+    searchMovie(searchMovieInput.value)
+})
+
+
+searchForm2.addEventListener('input', e => {    
+    const input = e.target
+    if(input.value.length >= 2) {
+        clearTimeout(searchMovieCooldown)
+        searchMovieCooldown = setTimeout(() => {
+            searchMovie(input.value)
+        }, 2000)
+    }
+})
+
+searchFormAjax.addEventListener('input', e => { 
+    e.preventDefault()   
+    const input = e.target
+    searchMovieAjax(input.value)
+})
+
+
 async function getDetails(id) {
     try {
         isLoading = true
@@ -69,9 +145,10 @@ async function getDetails(id) {
             month: 'long',
             year: 'numeric'
         })
+        pagination.classList.add('hidden-2')
         mainDOM.innerHTML = ''
         mainDOM.insertAdjacentHTML('beforeend', `
-            <button class="back-button" onclick="getTopRatedMovies(1)">Назад</button> d
+            <button class="back-button" onclick="backToMovies()">Назад</button> 
             <div class="details-page">
                 <div class="movie-details">
                     <div class="movie-details-img-wrapper">
@@ -100,6 +177,11 @@ async function getDetails(id) {
     } catch (e) {
         console.error(e);
     }
+}
+
+function backToMovies() {
+    pagination.classList.remove('hidden-2')
+    getTopRatedMovies(1)
 }
 
 function changePages() {
@@ -141,14 +223,14 @@ async function loadPage() {
 
 loadPage()
 
-window.onscroll = function(e) {
-    const scrollPosition = window.scrollY + window.innerHeight
-    const pageHeight = document.documentElement.scrollHeight
+// window.onscroll = function(e) {
+//     const scrollPosition = window.scrollY + window.innerHeight
+//     const pageHeight = document.documentElement.scrollHeight
 
-    if(!isLoading && scrollPosition >= pageHeight - 300) {
-        if(currentPage < totalPages) {
-            getTopRatedMovies(currentPage + 1, false)
-        }
-    }
-}
+//     if(!isLoading && scrollPosition >= pageHeight - 300) {
+//         if(currentPage < totalPages) {
+//             getTopRatedMovies(currentPage + 1, false)
+//         }
+//     }
+// }
 
